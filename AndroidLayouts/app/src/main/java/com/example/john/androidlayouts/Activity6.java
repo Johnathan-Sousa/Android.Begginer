a7d75db8a1e0f17d622c2249bfc7b17c52636ecf
Johnathan-Sousa <jm.sousa00@gmail.com>
2016-08-08 16:40:02 -0300

    Revert "Revert "Código de Interfaces Android""
    This reverts commit 7e462ac1c8b1a0b452cfddf99b67e0216bc280cc.

---
+++ AndroidLayouts/app/src/main/java/com/example/john/androidlayouts/Activity6.java
@@ -0,0 +1,14 @@
+package com.example.john.androidlayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Activity6 extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scroll_activity6);
+    }
+}
